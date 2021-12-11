fa402092570ee9b1af75264f368e82896e41e7e8
Tee55 <teerapaths@gmail.com>
2021-12-11 19:10:33 +0700

    main

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
+from numpy.lib.function_base import append
 import pandas as pd
 import os
 import numpy as np
@@ -9,11 +10,17 @@ from scipy.signal import butter, lfilter
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn import preprocessing 
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import preprocessing
+from itertools import combinations
+from tensorflow.keras.layers import Input, Dense, Conv1D, Dropout, Flatten, MaxPooling1D, BatchNormalization, ReLU, GlobalAveragePooling1D
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import TensorBoard
+import shutil
+
 
 class EEGutil:
-    
+
     def __init__(self):
         self.batch_size = 1
         self.DATASET_DIR = "dataset/"
@@ -36,43 +43,30 @@ class EEGutil:
         Pxx_pow = np.square(Pxx_abs)
         Pxx_sum = sum(Pxx_pow[ind_min: ind_max])
         return Pxx_sum
-            
+
     def segmentation(self, x, time_length, time_shift):
         n = time_length * self.sampling_rate  # group size
         m = time_shift * self.sampling_rate  # overlap size
         return [x[i:i+n] for i in range(0, len(x), n-m)]
-    
+
     def extract_features(self, x):
         epoches = self.segmentation(x, self.duration, self.time_shift)
-        
-        delta_avg = []
-        theta_avg = []
-        alpha_avg = []
-        beta_avg = []
-        gamma_avg = []
-        total_power_avg = []
+
+        delta = theta = alpha = beta = gamma = total_power = []
         for epoch in epoches:
-            delta = self.bandpower(epoch, 1, 4)
-            theta = self.bandpower(epoch, 4, 8)
-            alpha = self.bandpower(epoch, 8, 13)
-            beta = self.bandpower(epoch, 13, 30)
-            gamma = self.bandpower(epoch, 30, 45)
-            
-            delta_avg.append(delta)
-            theta_avg.append(theta)
-            alpha_avg.append(alpha)
-            beta_avg.append(beta)
-            gamma_avg.append(gamma)
-            
-        delta_avg = np.mean(np.array(delta_avg))
-        theta_avg = np.mean(np.array(theta_avg))
-        alpha_avg = np.mean(np.array(alpha_avg))
-        beta_avg = np.mean(np.array(beta_avg))
-        gamma_avg = np.mean(np.array(gamma_avg))
-        
-        features = [delta_avg, theta_avg, alpha_avg, beta_avg, gamma_avg]
-                
-        return features
+            delta_val = self.bandpower(epoch, 1, 4)
+            theta_val = self.bandpower(epoch, 4, 8)
+            alpha_val = self.bandpower(epoch, 8, 13)
+            beta_val = self.bandpower(epoch, 13, 30)
+            gamma_val = self.bandpower(epoch, 30, 45)
+
+            delta.append(delta_val)
+            theta.append(theta_val)
+            alpha.append(alpha_val)
+            beta.append(beta_val)
+            gamma.append(gamma_val)
+
+        return delta, theta, alpha, beta, gamma
 
     def cal_acc(self, predictions, y_val):
         score = 0
@@ -81,16 +75,20 @@ class EEGutil:
                 score += 1
         acc = score/len(predictions)
         return acc
-    
+
     def load_dataset(self):
-        
+
         x = []
         y = []
-        
+
+        channels = ["O1", "O2"]
+
         for id_dir in os.listdir(self.DATASET_DIR):
             for i in range(0, self.n_samples):
-                data_file = os.listdir(os.path.join(self.DATASET_DIR, id_dir))[i]
-                general_info_df = pd.read_csv(os.path.join(self.DATASET_DIR, id_dir, data_file), delimiter=",", header=None, nrows=1, skipinitialspace=True)
+                data_file = os.listdir(
+                    os.path.join(self.DATASET_DIR, id_dir))[i]
+                general_info_df = pd.read_csv(os.path.join(
+                    self.DATASET_DIR, id_dir, data_file), delimiter=",", header=None, nrows=1, skipinitialspace=True)
                 general_info = {}
                 for col in general_info_df.columns:
                     text = general_info_df.iloc[0][col]
@@ -100,57 +98,188 @@ class EEGutil:
 
                 headers = general_info["labels"]
                 header_list = headers.split(" ")
-                
-                df = pd.read_csv(os.path.join(self.DATASET_DIR, id_dir, data_file), delimiter=",", names=header_list, skiprows=1, skipinitialspace=True)
+
+                df = pd.read_csv(os.path.join(self.DATASET_DIR, id_dir, data_file),
+                                 delimiter=",", names=header_list, skiprows=1, skipinitialspace=True)
                 df = df.drop(df.index[self.sampling_rate * self.duration:])
-                x.append(df["O1"])
+                x.append([df[channel] for channel in channels])
                 y.append(id_dir)
-            
+
+        print(headers)
+        
+        self.num_people = len(os.listdir(self.DATASET_DIR))
+
         x = np.array(x)
         y = self.label_encoder.fit_transform(y)
-                
+        
+        y = np.reshape(y, (-1, 1))
+
         return x, y
-    
-    def preprocessing(self, x):
-        
+
+    def preprocessing(self, x, combi=None):
         new_x = []
-        for series in x:
-            features = self.extract_features(series)
-            new_x.append(features)
+        for sample in x:
+            delta_avg = theta_avg = alpha_avg = beta_avg = gamma_avg = []
+            for channel in sample:
+                delta, theta, alpha, beta, gamma = self.extract_features(
+                    channel)
+                delta_avg.append(delta)
+                theta_avg.append(theta)
+                alpha_avg.append(alpha)
+                beta_avg.append(beta)
+                gamma_avg.append(gamma)
+
+            delta_avg = np.array(delta_avg)
+            theta_avg = np.array(theta_avg)
+            alpha_avg = np.array(alpha_avg)
+            beta_avg = np.array(beta_avg)
+            gamma_avg = np.array(gamma_avg)
+
+            delta_avg = np.mean(delta_avg)
+            theta_avg = np.mean(theta_avg)
+            alpha_avg = np.mean(alpha_avg)
+            beta_avg = np.mean(beta_avg)
+            gamma_avg = np.mean(gamma_avg)
+
+            features = np.array([delta_avg, theta_avg,
+                                alpha_avg, beta_avg, gamma_avg])
             
+            if combi != None:
+                features = np.array([features[index] for index in combi])
+                
+            new_x.append(features)
+
         x = np.array(new_x)
-        
-        # Normalization
-        x = preprocessing.normalize(x)
-        
+
         df = pd.DataFrame(x)
-        print(df)
-             
+        #print(df)
+
         return x
+    
+    def fisher(self, x, y):
         
-    def main(self):
-        x, y = self.load_dataset()
         x = self.preprocessing(x)
         
-        print(x.shape, y.shape)
-        
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
-        
-        knn = KNeighborsClassifier(n_neighbors=5)
-        knn.fit(X_train, y_train)
-        
-        lda = LinearDiscriminantAnalysis()
-        lda.fit(X_train, y_train)
-        
-        predictions = knn.predict(X_test)
-        acc = accuracy_score(y_test, predictions)
-        print("KNN Accuracy: {}".format(acc))
-        
-        predictions = lda.predict(X_test)
-        acc = accuracy_score(y_test, predictions)
-        print("LDA Accuracy: {}".format(acc))
+        # Fisher score
+        fisher_score,_ = chi2(x, y)
+        idx = np.argsort(fisher_score)
+        idx = idx[::-1]
+        return idx
     
+    
+    def create_model(self, input_shape):
+        input_layer = Input(input_shape)
+        conv1 = Conv1D(filters=64, kernel_size=3, padding="same")(input_layer)
+        conv1 = BatchNormalization()(conv1)
+        conv1 = ReLU()(conv1)
+
+        conv2 = Conv1D(filters=64, kernel_size=3, padding="same")(conv1)
+        conv2 = BatchNormalization()(conv2)
+        conv2 = ReLU()(conv2)
+
+        conv3 = Conv1D(filters=64, kernel_size=3, padding="same")(conv2)
+        conv3 = BatchNormalization()(conv3)
+        conv3 = ReLU()(conv3)
+
+        gap = GlobalAveragePooling1D()(conv3)
+
+        output_layer = Dense(self.num_people, activation="softmax")(gap)
+
+        return Model(inputs=input_layer, outputs=output_layer)
+
+    def feature_selection(self, x, y):
+        idx = self.fisher(x, y)
+        print("Fisher Features Index (High -> Low): {}".format(idx))
+        
+        # Feature selection
+        all_acc = []
+        all_combi = []
+        
+        for combi_number in range(1, len(idx)+1):
+            combi_idx = combinations(idx, combi_number)
+            combi_idx = list(combi_idx)
+            
+            for combi in combi_idx:
+                
+                data_x = self.preprocessing(x, combi)
+                
+                X_train, X_test, y_train, y_test = train_test_split(
+                    data_x, y, test_size=0.33)
+
+                knn = KNeighborsClassifier(n_neighbors=5)
+                knn.fit(X_train, y_train)
+                
+                print("=====Combination {}=====".format(combi))
+
+                predictions = knn.predict(X_test)
+                acc = accuracy_score(y_test, predictions)
+                print("KNN Accuracy: {}".format(acc))
+                
+                all_acc.append(acc)
+                all_combi.append(combi)
+                
+        all_acc = np.array(all_acc)
+                
+        print("Highest Accuracy: {}".format(np.max(all_acc)))
+        print("With Combination: {}".format(all_combi[np.argmax(all_acc)]))
+        
+        return all_combi[np.argmax(all_acc)]
+    
+    def filter(self, x, low_fs, high_fs):
+        b, a = butter(5, [low_fs, high_fs], btype='band', fs=self.sampling_rate)
+        channel = lfilter(b, a, x)
+        return x
+    
+    def cleanTensorflowLogs(self):
+        LOG_DIR = ['logs/']
+        for logdir in LOG_DIR:
+            subfolders = [os.path.join(logdir, f) for f in os.listdir(
+                logdir) if not f.endswith(('.csv', '.gitignore'))]
+            for subfolder in subfolders:
+                if os.path.isdir(subfolder):
+                    shutil.rmtree(subfolder)
+                else:
+                    os.remove(subfolder)
+        
+    def main(self):
+        
+        self.cleanTensorflowLogs()
+        
+        x, y = self.load_dataset()
+        print("Original Shape: {}, {}".format(x.shape, y.shape))
+        
+        
+        new_x = []
+        for sample in x:
+            data = []
+            for channel_1, channel_2 in zip(sample[0], sample[1]):
+                data.append([channel_1, channel_2])
+                
+            data = np.array(data)
+            data = preprocessing.normalize(data)
+            
+            new_x.append(data)
+
+        x = np.array(new_x)
+        
+        print("Train shape: {}, {}".format(x.shape, y.shape))
+        
+        tensorboard = TensorBoard(log_dir='logs', update_freq='batch')
+        callbacks = [tensorboard]
+        
+        model = self.create_model(input_shape=x.shape[1:])
+        model.compile(
+            optimizer="adam",
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+        model.fit(x, y, batch_size=self.batch_size, epochs=350, callbacks=callbacks)
+        model.save_weights(os.path.join("model", "model.h5"))
+        
+        predictions = model.predict(x)
+        acc = accuracy_score(y, predictions)
+        print("Accuracy: {}".format(acc))
+        
 if __name__ == '__main__':
     eegutil = EEGutil()
     eegutil.main()
-    
