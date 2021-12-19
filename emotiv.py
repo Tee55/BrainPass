613528b4b617409ecb115e3714946dcad2261c32
Tee55 <teerapaths@gmail.com>
2021-12-19 22:03:47 +0700

    main

--- emotiv.py
+++ emotiv.py
@@ -1,12 +1,4 @@
 import os
-import platform
-import sys
-import select
-from array import *
-from ctypes import *
-import logging
-import threading
-import timeit
 
 class EEG_device():
     def __init__(self) -> None:
